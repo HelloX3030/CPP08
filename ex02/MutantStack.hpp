c12d53cb287887ea095c3c79f6c8038348c04805
Lukas Seeger <lseeger@.42heilbronn.de>
2025-11-26 16:13:42 +0100

    Butchered Mutant Stack tests

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -1,8 +1,18 @@
 #pragma once
 
-class MutantStack
-{
+#include <stack>
 
+template<typename T>
+class MutantStack : public std::stack<T>
+{
+    private:
+    public:
+        // Iterator Types
+        typedef typename std::stack<T>::container_type::iterator iterator;
+
+        // Iterator Functions
+        iterator begin();
+        iterator end();
 };
 
 #include "MutantStack.tpp"

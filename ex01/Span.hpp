741951ae0a82190a6b8f45debadeb2fe1ade7f30
Lukas Seeger <lseeger@.42heilbronn.de>
2025-12-10 13:35:40 +0100

    Span.tpp

--- ex01/Span.hpp
+++ ex01/Span.hpp
@@ -31,12 +31,4 @@ class Span
         int longestSpan() const;
 };
 
-template <typename It>
-void Span::addRange(It begin, It end)
-{
-    int count = std::distance(begin, end);
-    if (_data.size() + count > _n)
-        throw std::out_of_range("Adding range exceeds Span capacity");
-
-    _data.insert(_data.end(), begin, end);
-}
+#include "Span.tpp"

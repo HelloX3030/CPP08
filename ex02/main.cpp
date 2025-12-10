1563f250d70ce1ac3c40a8e317f77438f46b16b3
Lukas Seeger <lseeger@.42heilbronn.de>
2025-12-10 13:44:47 +0100

    more mutant stack tests

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -30,8 +30,8 @@ int main(void)
         std::stack<int> s(mstack);   
     }
 
-    std::cout << "List test:" << std::endl;
     std::cout << "------------------------------------------" << std::endl;
+    std::cout << "List test:" << std::endl;
     {
         std::list<int> mstack;
         mstack.push_back(5);
@@ -52,6 +52,56 @@ int main(void)
         {
             std::cout << *it << std::endl;
             ++it;
+        }
+    }
+
+    std::cout << "------------------------------------------" << std::endl;
+    std::cout << "Const MutantStack test:" << std::endl;
+    {
+        MutantStack<int> mstack;
+        mstack.push(10);
+        mstack.push(20);
+        mstack.push(30);
+        const MutantStack<int> const_mstack = mstack;
+        MutantStack<int>::const_iterator it = const_mstack.begin();
+        MutantStack<int>::const_iterator ite = const_mstack.end();
+        while (it != ite)
+        {
+            std::cout << *it << std::endl;
+            ++it;
+        }
+    }
+
+    std::cout << "------------------------------------------" << std::endl;
+    std::cout << "Reverse MutantStack test:" << std::endl;
+    {
+        MutantStack<int> mstack;
+        mstack.push(100);
+        mstack.push(200);
+        mstack.push(300);
+        MutantStack<int>::reverse_iterator rit = mstack.rbegin();
+        MutantStack<int>::reverse_iterator rite = mstack.rend();
+        while (rit != rite)
+        {
+            std::cout << *rit << std::endl;
+            ++rit;
+        }
+    }
+
+    std::cout << "------------------------------------------" << std::endl;
+    std::cout << "Const Reverse MutantStack test:" << std::endl;
+    {
+        MutantStack<int> mstack;
+        mstack.push(1000);
+        mstack.push(2000);
+        mstack.push(3000);
+        const MutantStack<int> const_mstack = mstack;
+        MutantStack<int>::const_reverse_iterator rit = const_mstack.rbegin();
+        MutantStack<int>::const_reverse_iterator rite = const_mstack.rend();
+        while (rit != rite)
+        {
+            std::cout << *rit << std::endl;
+            ++rit;
         }
     }
     return 0;
